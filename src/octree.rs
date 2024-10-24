c84c4fbe609c35ab6c5a1d66b01e007f0b85a73a
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2024-10-24 15:08:36 -0700

    created subdivision for octree.rs

--- src/octree.rs
+++ src/octree.rs
@@ -3,24 +3,24 @@ use crate::body::Body;
 
 #[derive(Debug)]
 pub struct Octree{
-    pub top_left_back_corner:[f32;3],
-    pub bottom_right_front_corner:[f32;3],
+    pub left_top_back_corner:Vec3,
+    pub right_bottom_front_corner:Vec3,
     pub body: Option<Body>,
     pub subregions: Option<[Box<Octree>;8]>,
-    pub center:[f32;3],
+    pub center:Vec3,
     pub center_of_mass:Vec3,
     pub total_mass: f32,
 }
 
 
 impl Octree{
-    pub fn new(left_top_back_corner: [f32;3], right_bottom_front_corner: [f32;3]) -> Octree{
+    pub fn new(corners: [Vec3;2]) -> Octree{
         Self {
-            top_left_back_corner,
-            bottom_right_front_corner,
+            left_top_back_corner: corners[0],
+            right_bottom_front_corner: corners[1],
             body: None,
             subregions: None,
-            center: Self::calculate_center(left_top_back_corner, right_bottom_front_corner),
+            center: Self::calculate_center(corners[0], corners[1]),
             center_of_mass: Vec3::new(0.0,0.0,0.0),
             total_mass: 0.0,
 
@@ -28,22 +28,87 @@ impl Octree{
     }
 
 
-    pub fn calculate_center(top_left_back_corner: [f32;3], bottom_right_front_corner: [f32;3]) -> [f32;3] {
-        let width_h: f32 = (bottom_right_front_corner[0] - top_left_back_corner[0]).abs() / 2.0;
-        let height_h: f32 = (bottom_right_front_corner[1] - top_left_back_corner[1]).abs() / 2.0;
-        let depth_h: f32 = (bottom_right_front_corner[2] - top_left_back_corner[2]).abs() / 2.0;
-        let center: [f32;3] = [top_left_back_corner[0] + width_h,top_left_back_corner[1] + height_h,top_left_back_corner[2] + depth_h];
-        return center;
+    pub fn calculate_center(top_left_back_corner: Vec3, bottom_right_front_corner: Vec3) -> Vec3 {
+        // let differences = (bottom_right_front_corner + top_left_back_corner) * 0.5;
+        // let center: Vec3 = Vec3::new(top_left_back_corner[0] + differences.x,top_left_back_corner[1] + differences.y,top_left_back_corner[2] + differences.z);
+        // return center;
+        return (bottom_right_front_corner + top_left_back_corner) * 0.5;
     }
 
     pub fn subdivide(&mut self){
         // this assumes the octs are perfect cubes and will not be negative
         // the simulation will not scale this time
         // we will think of other ways to solve this
-        let length: f32 = (self.bottom_right_front_corner[0] - self.top_left_back_corner[0]).abs() / 2.0;
-        // let left_bottom_front_cube: [[f32;3];2] = [
-        //     [self.top_left_back_corner[0],],
-        //     [] ];
+        // let length: f32 = (self.right_bottom_front_corner.x - self.left_top_back_corner.x).abs() / 2.0;
+
+
+        self.subregions = Some([
+            //A
+            Box::new(Octree::new(
+                [Vec3::new(self.left_top_back_corner.x, self.right_bottom_front_corner.y, self.center.z),
+                    Vec3::new(self.center.x, self.center.y, self.right_bottom_front_corner.z)])),
+
+            //B
+            Box::new(Octree::new(
+                [
+                    Vec3::new(self.center.x, self.left_top_back_corner.y, self.center.z),
+                    Vec3::new(self.right_bottom_front_corner.x, self.center.y, self.right_bottom_front_corner.z)
+                ]
+            )),
+
+            //C
+            Box::new(Octree::new(
+                [
+                    Vec3::new(self.left_top_back_corner.x, self.center.y, self.center.z),
+                    Vec3::new(self.center.x, self.right_bottom_front_corner.y, self.right_bottom_front_corner.z)
+                ]
+            )),
+
+            //D
+            Box::new(Octree::new(
+                [
+                    Vec3::new(self.center.x, self.center.y, self.center.z),
+                    Vec3::new(self.right_bottom_front_corner.x, self.left_top_back_corner.y, self.right_bottom_front_corner.z)
+                ]
+            )),
+
+            //E
+            Box::new(Octree::new(
+                [
+                    Vec3::new(self.left_top_back_corner.x, self.left_top_back_corner.y, self.left_top_back_corner.z),
+                    Vec3::new(self.center.x, self.center.y, self.center.z)
+                ]
+            )),
+
+            //F
+            Box::new(Octree::new(
+                [
+                    Vec3::new(self.center.x, self.left_top_back_corner.y, self.left_top_back_corner.z),
+                    Vec3::new(self.right_bottom_front_corner.x, self.center.y, self.center.z)
+                ]
+            )),
+
+            //G
+            Box::new(Octree::new(
+                [
+                    Vec3::new(self.left_top_back_corner.x, self.center.y, self.left_top_back_corner.z),
+                    Vec3::new(self.center.x, self.right_bottom_front_corner.y, self.center.z)
+                ]
+            )),
+
+            //H
+            Box::new(Octree::new(
+                [
+                    Vec3::new(self.center.x, self.center.y, self.left_top_back_corner.z),
+                    Vec3::new(self.right_bottom_front_corner.x, self.right_bottom_front_corner.y, self.center.z)
+                ]
+            ))
+        ]);
+
+
+
+
+
     }
 
 
@@ -54,12 +119,23 @@ impl Octree{
 #[cfg(test)]
 mod tests{
     use crate::octree::Octree;
+    use glam::{ Vec3};
     #[test]
     fn oct_center(){
-        let tlbc = [5.0,5.0,5.0];
-        let brfc = [10.0,10.0,10.0];
-        let center:[f32;3] = Octree::calculate_center(tlbc,brfc);
-        assert!(center==[7.5,7.5,7.5]);
-        assert!(Octree::calculate_center([0.0,0.0,0.0],[20.0,20.0,20.0]) == [10.0,10.0,10.0]);
+        // let tlbc = [5.0,5.0,5.0];
+        // let brfc = [10.0,10.0,10.0];
+        let corners = [
+          Vec3::new(5.0,5.0,5.0),
+            Vec3::new(10.0,10.0,10.0),
+        ];
+        let center = Octree::calculate_center(Vec3::new(5.0,5.0,5.0), Vec3::new(10.0,10.0,10.0));
+        let expected_center = Vec3::new(7.5,7.5,7.5);
+        assert_eq!(center, expected_center);
+        // assert!(Octree::calculate_center([0.0,0.0,0.0],[20.0,20.0,20.0]) == [10.0,10.0,10.0]);
+    }
+
+    #[test]
+    fn oct_subdivide(){
+
     }
 }
